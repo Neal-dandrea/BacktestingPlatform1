3806b380b3d5944822f5be26f226fd248ba3a5de
mrussotti <matthewlax24@yahoo.com>
2025-01-04 18:05:59 -0500

    amplify documents

--- backtesting-platform-temp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ backtesting-platform-temp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "backtestingAPI": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "backtestingLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
